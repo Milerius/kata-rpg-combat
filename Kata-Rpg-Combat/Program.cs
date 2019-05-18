c9828ad2ee94c22e5ec705b5f81b613e267322a3
Roman Sztergbaum <rmscastle@gmail.com>
2019-05-18 09:26:09 +0200

    Create kata-rpg repository

---
+++ Kata-Rpg-Combat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata_Rpg_Combat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
