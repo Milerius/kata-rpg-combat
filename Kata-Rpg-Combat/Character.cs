ff3532e9f595e468e56a91fe5f45acaac756876a
Roman Sztergbaum <rmscastle@gmail.com>
2019-05-19 01:57:32 +0200

    style: refactoring code, more concise

--- Kata-Rpg-Combat/Character.cs
+++ Kata-Rpg-Combat/Character.cs
@@ -11,13 +11,12 @@ namespace Kata_Rpg_Combat
         {
             if (MathUtilility.GetDistance(WorldPosition, other.WorldPosition) > _maxRange ||
                 ReferenceEquals(this, other)) return;
-            var isLevelAboveByMaxLevelDifference = (int) Level - (int) other.Level >= MaxLevelDifference;
-            var isLevelBehindByMaxLevelDifference = (int) other.Level - (int) Level >= MaxLevelDifference;
-            if (isLevelAboveByMaxLevelDifference) damages += damages / 2;
-            else if (isLevelBehindByMaxLevelDifference) damages -= damages / 2;
-            var isDamagesExceedOtherHealth = damages >= other.Health;
-
-            if (isDamagesExceedOtherHealth)
+            bool IsLevelBehindByMaxLevelDifference() => (int) other.Level - (int) Level >= MaxLevelDifference;
+            bool IsLevelAboveByMaxLevelDifference() => (int) Level - (int) other.Level >= MaxLevelDifference;
+            bool IsDamagesExceedOtherHealth() => damages >= other.Health;
+            if (IsLevelAboveByMaxLevelDifference()) damages += damages / 2;
+            else if (IsLevelBehindByMaxLevelDifference()) damages -= damages / 2;
+            if (IsDamagesExceedOtherHealth())
             {
                 other.Health = 0;
                 other.State = CharacterState.Dead;
@@ -28,42 +27,36 @@ namespace Kata_Rpg_Combat
 
         public void Heal(uint heal)
         {
-            if (State == CharacterState.Dead)
-                return;
-            if (Health + heal > MaxHealth)
-            {
-                Health = MaxHealth;
-            }
-            else
-            {
-                Health += heal;
-            }
+            if (State == CharacterState.Dead) return;
+            bool HealExceedMaxHealth() => Health + heal > MaxHealth;
+            if (HealExceedMaxHealth()) Health = MaxHealth;
+            else Health += heal;
         }
 
-        private readonly uint _maxRange;
-        public Point WorldPosition { get; set; } = Point.Empty;
-
+        //! Constructor
         protected Character(uint maxRange)
         {
             _maxRange = maxRange;
         }
 
-        public uint Health { get; set; } = MaxHealth;
+
+        //! Relational Operation
+        private bool Equals(Character other) =>
+            Health == other.Health && Level == other.Level && State == other.State &&
+            _maxRange == other._maxRange && WorldPosition == other.WorldPosition;
+
+        public override bool Equals(object obj) =>
+            !ReferenceEquals(null, obj) &&
+            (ReferenceEquals(this, obj) || obj.GetType() == GetType() && Equals((Character) obj));
+
+
+        //! Private members
+        private readonly uint _maxRange;
+
+        //! Properties
+        public Point WorldPosition { get; set; } = Point.Empty;
+        public uint Health { get; private set; } = MaxHealth;
         public uint Level { get; set; } = 1;
-
-        private bool Equals(Character other)
-        {
-            return Health == other.Health && Level == other.Level && State == other.State;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((Character) obj);
-        }
-
-        public CharacterState State { get; set; } = CharacterState.Alive;
+        public CharacterState State { get; private set; } = CharacterState.Alive;
     }
 }
